e590d6d3f3a194eb0b9b556557fab3e30f6dff59
MarkoTunjic <marko.tunjic21@gmail.com>
2023-10-03 21:46:46 +0200

    Add needed queries for movie filtering

--- web-api/src/main/java/com/example/demo/persistance/MovieRepository.java
+++ web-api/src/main/java/com/example/demo/persistance/MovieRepository.java
@@ -5,6 +5,19 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.models.Movie;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByNameContaining(String name);
+
+    List<Movie> findByAddingDateBetween(Date start, Date end);
+
+    List<Movie> findByGenreNameIn(List<String> genreNames);
+
+    List<Movie> findByNameContainingAndAddingDateBetweenAndGenreNameIn(String movieName, Date start, Date end,
+            List<String> genreNames);
+
+    List<Movie> findByNameContainingAndAddingDateBetween(String movieName, Date start, Date end);
 }
