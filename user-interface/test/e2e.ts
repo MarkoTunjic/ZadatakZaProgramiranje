cf4fdab19498301470d719212cda980d6ac95a17
MarkoTunjic <marko.tunjic21@gmail.com>
2023-10-07 04:13:38 +0200

    add tests for genre search

--- user-interface/test/e2e.ts
+++ user-interface/test/e2e.ts
@@ -1,7 +1,5 @@
 import { NightwatchAPI, NightwatchTests, assert, NightwatchBrowser } from 'nightwatch';
 
-
-
 const home: NightwatchTests = {
     "Table visible test": () => {
         browser.url("http://localhost:4200").assert.visible("#movies-table");
@@ -19,14 +17,60 @@ const home: NightwatchTests = {
         browser.waitForElementVisible("#submit-button", 500)
             .click("#submit-button");
 
-        const className = '.cdk-column-name'; // Replace with your actual class name
-        while (!await browser.getAttribute(".mat-mdc-paginator-navigation-next", "disabled")) {
+        const className = '.cdk-column-name';
+        let disabled = await browser.getAttribute(".mat-mdc-paginator-navigation-next", "disabled")
+        do {
             (await browser.findElements(className)).forEach(async (element, index) => {
                 if (index > 0)
                     browser.assert.textMatches('tbody>:nth-child(' + (index) + ')>:nth-child(2)', new RegExp(searchQuery, 'i'))
             });
-            browser.click(".mat-mdc-paginator-navigation-next");
+            if (!disabled)
+                browser.click(".mat-mdc-paginator-navigation-next");
+        } while (!(disabled = await browser.getAttribute(".mat-mdc-paginator-navigation-next", "disabled")));
+    },
+
+    "Genre search test": async () => {
+        browser.url("http://localhost:4200");
+
+        const genres = ["Comedy", "Horror"]
+        browser.waitForElementVisible("#dropdown").click("#dropdown");
+
+        browser.waitForElementVisible("#dropdown-panel", 500)
+        for (let genre of genres) {
+            browser.click("mat-option[ng-reflect-value=" + genre + "]")
         }
+
+        browser.click(".cdk-overlay-backdrop");
+
+        browser.waitForElementVisible("#submit-button", 500)
+            .click("#submit-button");
+
+        const className = '.cdk-column-genreName';
+        let disabled = await browser.getAttribute(".mat-mdc-paginator-navigation-next", "disabled")
+        do {
+            (await browser.findElements(className)).forEach(async (element, index) => {
+                let currentGenre: string = await browser.elementIdText(element.getId());
+                currentGenre = await browser.elementIdText(element.getId())//for some reason it gotta be done twice
+                if (index > 0) {
+                    let found = false;
+                    for (let genre of genres) {
+                        if (currentGenre === genre) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        assert.fail("Genre not in filtered genres");
+                    } else {
+                        assert.ok("true");
+                    }
+                }
+
+
+            });
+            if (!disabled)
+                browser.click(".mat-mdc-paginator-navigation-next");
+        } while (!(disabled = await browser.getAttribute(".mat-mdc-paginator-navigation-next", "disabled")));
     }
 }
 
